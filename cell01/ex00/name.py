95793df09db4887f38a74fbd5cca08cd615c9b16
Phinyapat2005 <inging161248@gmail.com>
2024-11-20 18:05:26 +0000

    finish

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+irst_name = "Phiypat"
+last_name = "Banbua"
+print(first_name+" "+last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Phiypat"
+last_name = "Banbua"
+whole_name = first_name+" "+last_name
+print(whole_name)
